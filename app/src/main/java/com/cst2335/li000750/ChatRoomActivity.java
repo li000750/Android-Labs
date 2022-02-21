6ea56c575bbe159352f5f376e9d6828f0895e718
Qin Li <li000750@algonquinlive.com>
2022-02-21 00:53:56 -0500

    Lab5 first version.

--- app/src/main/java/com/cst2335/li000750/ChatRoomActivity.java
+++ app/src/main/java/com/cst2335/li000750/ChatRoomActivity.java
@@ -3,6 +3,10 @@ package com.cst2335.li000750;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -20,14 +24,22 @@ import java.util.ArrayList;
 public class ChatRoomActivity extends AppCompatActivity {
 
     private static final String TAG = "ChatRoomActivity";//print log
+    MyOpenHelper myOpener;
+    SQLiteDatabase theDatabase;
+
     //constructor Messages
     public static class Messages {
         String inputMessage;
         public Boolean sendOrReceive;
+        long id;
 
-        private Messages(String inMessage, Boolean choiceSendOrReceive) {
+        public Messages(String inMessage, Boolean choiceSendOrReceive, long _id) {
             this.inputMessage = inMessage;
             this.sendOrReceive = choiceSendOrReceive;
+            this.id = _id;
+        }
+        public long getId() {
+            return id;
         }
     }
     ArrayList<Messages> myMessages = new ArrayList<>();
@@ -38,6 +50,25 @@ public class ChatRoomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
+
+        myOpener = new MyOpenHelper( this );
+        //open the database:
+        theDatabase = myOpener.getWritableDatabase();
+        Cursor results = theDatabase.rawQuery( "Select * from " + MyOpenHelper.TABLE_NAME + ";", null );
+        int idIndex = results.getColumnIndex( MyOpenHelper.COL_ID );
+        int  messageIndex = results.getColumnIndex( MyOpenHelper.COL_MESSAGE);
+        int sOrRIndex = results.getColumnIndex( MyOpenHelper.COL_SEND_RECEIVE);
+
+        //cursor is pointing to row -1
+        while( results.moveToNext() ) //returns false if no more data
+        { //pointing to row 2
+            int id = results.getInt(idIndex);
+            String message = results.getString( messageIndex );
+            boolean sendOrRecieve = results.getInt(sOrRIndex)==1;
+
+            //add to arrayList:
+            myMessages.add( new Messages( message,sendOrRecieve, id ));
+        }
 
         //attach listview to adapter
         ListView myList = findViewById(R.id.listView);
@@ -52,8 +83,16 @@ public class ChatRoomActivity extends AppCompatActivity {
         Button sendButton = findViewById(R.id.button1);
         sendButton.setOnClickListener( click -> {
             String inputText = editText.getText().toString();
+            ContentValues newRow = new ContentValues();
+            //Message column:
+            newRow.put( MyOpenHelper.COL_MESSAGE , inputText );
+            //Send or receive column:
+            newRow.put(MyOpenHelper.COL_SEND_RECEIVE, 1);
+            //now that columns are full, you insert:
+            long id = theDatabase.insert( MyOpenHelper.TABLE_NAME, null, newRow );
+
             Log.i(TAG, "Adding a sending row");
-            myMessages.add(new Messages(inputText,true));
+            myMessages.add(new Messages(inputText,true,id));
             editText.setText("");
             myAdapter.notifyDataSetChanged();
         });
@@ -61,24 +100,33 @@ public class ChatRoomActivity extends AppCompatActivity {
         Button receiveButton = findViewById(R.id.button2);
         receiveButton.setOnClickListener( click -> {
             String inputText = editText.getText().toString();
+            ContentValues newRow = new ContentValues();
+            //Message column:
+            newRow.put( MyOpenHelper.COL_MESSAGE , inputText );
+            //Send or receive column:
+            newRow.put(MyOpenHelper.COL_SEND_RECEIVE, 1);
+            //now that columns are full, you insert:
+            long id = theDatabase.insert( MyOpenHelper.TABLE_NAME, null, newRow );
             Log.i(TAG, "Adding a receiving row");
-            myMessages.add(new Messages(inputText,false));
+            myMessages.add(new Messages(inputText,false,id));
             editText.setText("");
             myAdapter.notifyDataSetChanged();
         });
 
         myList.setOnItemLongClickListener( (p, b, pos, id) -> {
+            Messages whatWasClicked = myMessages.get(pos);
             AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
             alertDialogBuilder.setTitle("Do you want to delete this?")
 
                     .setIcon(0)
 
                     //What is the message:
-                    .setMessage("The selected row is:"+ (id+1)+"\n"+"The database id is:"+id)
+                    .setMessage("The selected row is:"+ pos+"\n"+"The database id is:"+id)
                     //what the Yes button does:
                     .setPositiveButton("Yes", (click, arg) -> {
                         myMessages.remove(pos);
                         myAdapter.notifyDataSetChanged();
+                        theDatabase.delete(MyOpenHelper.TABLE_NAME,MyOpenHelper.COL_ID+"=?",new String[]{Long.toString(whatWasClicked.getId())});
                     })
                     //What the No button does:
                     .setNegativeButton("No", (click, arg) -> { })
